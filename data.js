9e953cc6aea6bae5ab5f55911d9730dfd9d9a594
Viper <93407500+V1perino@users.noreply.github.com>
2022-07-25 01:33:12 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+const availableRadios = [
+    "RADIO_01_CLASS_ROCK",              // Los Santos Rock Radio                > Country Radio y
+    "RADIO_02_POP",                     // Non-Stop-Pop FM                      > Evropa 2 y
+    "RADIO_03_HIPHOP_NEW",              // Radio Los Santos                     > Europa 2 y
+    "RADIO_04_PUNK",                    // Channel X                            > Rádio Kiss y
+    "RADIO_05_TALK_01",                 // West Coast Talk Radio                > Frekvence 1
+    "RADIO_06_COUNTRY",                 // Rebel Radio                          > Impuls y
+    "RADIO_07_DANCE_01",                // Soulwax FM                           > Bonton y
+    "RADIO_08_MEXICAN",                 // East Los FM                          > Blaník y
+    "RADIO_09_HIPHOP_OLD",              // West Coast Classics                  > Expres y
+    "RADIO_11_TALK_02",                 // Blaine County Radio                  > nefunguje
+    "RADIO_12_REGGAE",                  // Blue Ark                             > Vlna y
+    "RADIO_13_JAZZ",                    // Worldwide FM                         > Expres SK y
+    "RADIO_14_DANCE_02",                // FlyLo FM                             > SK FUN y
+    "RADIO_15_MOTOWN",                  // The Lowdown 91.1                     > Hitradio City y
+    "RADIO_16_SILVERLAKE",              // Radio Mirror Park                    > Fajn Radio y
+    "RADIO_17_FUNK",                    // Space 103.2                          > Krokodýl y
+    "RADIO_18_90S_ROCK",                // Vinewood Boulevard Radio             > Rádiožurnál y
+    "RADIO_19_USER",                    // Self Radio                           > DEFAULT y
+    "RADIO_20_THELAB",                  // The Lab                              > ORION y
+    "RADIO_21_DLC_XM17",                // Blonded Los Santos 97.8 FM           > Melody SK y
+    "RADIO_22_DLC_BATTLE_MIX1_RADIO",   // Los Santos Underground Radio         > Viva y 
+    "RADIO_23_DLC_XM19_RADIO",          // iFruit Radio                         > Regina y
+    "RADIO_27_DLC_PRHEI4",              // Still Slipping Los Santos            > Dance Club
+    "RADIO_34_DLC_HEI4_KULT",           // Kult FM                              > Hitrádio contact y
+    "RADIO_35_DLC_HEI4_MLR",            // The Music Locker                     > Rádio Ukrajina
+    "RADIO_36_AUDIOPLAYER",             // Media Player                         > Signál rádio
+    "RADIO_37_MOTOMAMI"                 // MOTOMAMI Los Santos                  > Hitrádio desítka
+];
